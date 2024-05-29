14230a7ffd92e81388ca4115732339b09e72f281
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-05-29 14:10:13 +0900

    Widgetのタップ操作を検知できるようにした（GestureDetectorウィジェットを利用して、対象のウィジェットを包み込んだ）

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,18 @@ class Counter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
-        child: Center(
-            child: Text(
-          '$_count',
-          textDirection: TextDirection.ltr,
-        )));
+    return GestureDetector(
+        onTap: () {
+          print('tapped!');
+        },
+        child: Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+            child: Center(
+                child: Text(
+              '$_count',
+              textDirection: TextDirection.ltr,
+            ))));
   }
 }
